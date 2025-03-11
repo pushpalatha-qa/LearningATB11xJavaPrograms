88cfe54eb8d4c87930a7f9abf257648a95c1d029
rhksinghm <168119774+rhksinghm@users.noreply.github.com>
2025-03-11 12:56:10 +0530

    11-march-2025 my first commit

--- src/ex_01_Java_Basic/Lab001_HelloWorldProgram.java
+++ src/ex_01_Java_Basic/Lab001_HelloWorldProgram.java
@@ -1,7 +1,9 @@
 package ex_01_Java_Basic;
 
-public class HelloWorld {
+public class Lab001_HelloWorldProgram
+{
     public static void main(String[] args) {
-        System.out.println("HelloWorld");
+        System.out.println();
     }
+
 }
