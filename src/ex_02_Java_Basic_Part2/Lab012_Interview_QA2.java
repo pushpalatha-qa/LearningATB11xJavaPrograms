dbb7556cb55b2145a557164017d9c038788ff917
rhksinghm <168119774+rhksinghm@users.noreply.github.com>
2025-03-17 14:10:25 +0530

    11-march-2025 completed -commit  variables in java,task-table of 9

--- src/ex_02_Java_Basic_Part2/Lab012_Interview_QA2.java
+++ src/ex_02_Java_Basic_Part2/Lab012_Interview_QA2.java
@@ -5,7 +5,7 @@ public class Lab012_Interview_QA2 {
         int Enum= 12; //enum- is a keyword not allowed where as Enum can be allowed.
         int a_b=90;//valid
        // int enum = 10; -->not valid because enum is a keyword
-        int a_b = 89;
+
         // int water bottle = 500; //not valid because space is not allowed
         int abc$bcvf = 90;//valid
         int Lab013 = 123;//valid
