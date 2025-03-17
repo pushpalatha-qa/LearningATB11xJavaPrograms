7d58061778ebd6d2480743445c45c8c50bb599b3
rhksinghm <168119774+rhksinghm@users.noreply.github.com>
2025-03-17 14:16:54 +0530

    11-march-2025 completed -commit2  variables in java,task-table of 9

---
+++ src/ex_02_Java_Basic_Part2/Lab013_Boolean.java
@@ -0,0 +1,10 @@
+package ex_02_Java_Basic_Part2;
+
+public class Lab013_Boolean {
+    public static void main(String[] args) {
+        boolean is_pushpa_married = true;
+        boolean is_pushpa_married1 = false;
+        boolean pushpa_has_audi = false;
+        boolean long_name_long_name_long_name_long_name_long_name_long_name = true;
+    }
+}
---
+++ src/ex_02_Java_Basic_Part2/Lab014_Byte.java
@@ -0,0 +1,9 @@
+package ex_02_Java_Basic_Part2;
+
+public class Lab014_Byte {
+    public static void main(String[] args) {
+        int age = 78; //
+        byte age1 = 86; // as max age of a human is 122 so we can use byte type also.
+        byte num = 10; // binary value of 10 is 00001010 --1byte= 8 bits which can store max 127 value
+    }
+}
