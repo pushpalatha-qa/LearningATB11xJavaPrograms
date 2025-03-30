f16714db87ce612b9f61fbd10c512c36fd24d708
rhksinghm <168119774+rhksinghm@users.noreply.github.com>
2025-03-31 00:04:38 +0530

    14-march-2025 completed -increment/decrement operator,if statement,user input-cli,scanner class Task  completed-triangle classifier using if else statement, even odd using input scannerclass,max of 2 number using ternary operator

---
+++ src/ex_08_If_Statement/Lab_071_If_statement.java
@@ -0,0 +1,10 @@
+package ex_08_If_Statement;
+
+public class Lab_071_If_statement {
+    public static void main(String[] args) {
+        int age = 17;
+        if (age > 18) {
+            System.out.println("Yes you are allowed to vote!");
+        }
+    }
+}
---
+++ src/ex_08_If_Statement/Lab_074_If_Interview.java
@@ -0,0 +1,12 @@
+package ex_08_If_Statement;
+
+public class Lab_074_If_Interview {
+    public static void main(String[] args) {
+        int a = 10;
+        if (a == 4) {
+            System.out.println("haha");
+        } else {
+            System.out.println("hoho");
+        }
+    }
+}
