3d540a35c6ce4e48233caae357afa649e6833e3a
rhksinghm <168119774+rhksinghm@users.noreply.github.com>
2025-04-27 11:53:31 +0530

    test1

---
+++ src/ex_16_Arrays/Lab_140_Array_Creation.java
@@ -0,0 +1,20 @@
+package ex_16_Arrays;
+
+public class Lab_140_Array_Creation {
+    public static void main(String[] args) {
+     // Method 1
+        int[] marks = {1, 2, 3, 4, 5, 6};
+        System.out.println(marks[0]);
+
+        //Method 2- create the array
+        int[] mark2 = new int[5]; // Fixed Size
+        int mark3[] = new int[5]; // Fixed Size
+        int mark4 = 10;
+        String[] names = new String[3];
+        names[0] = "pramod";
+        names[1] = "Amit";
+        names[2] = "Yasho";
+        System.out.println(names[2]);
+
+    }
+}
