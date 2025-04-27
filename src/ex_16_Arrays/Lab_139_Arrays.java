ffd875ccbde2728e3def847f5213f74091474049
rhksinghm <168119774+rhksinghm@users.noreply.github.com>
2025-04-27 11:42:38 +0530

    test

---
+++ src/ex_16_Arrays/Lab_139_Arrays.java
@@ -0,0 +1,17 @@
+package ex_16_Arrays;
+
+public class Lab_139_Arrays {
+    public static void main(String[] args) {
+        int a = 10;
+        int [] marks = {91,90,51,100,91,92,89};
+        boolean [] ismarried_people = {true,true,false};
+        System.out.println(marks.length);
+        System.out.println(marks[0]);
+        System.out.println(marks[4]);
+//        System.out.println(marks[-1]); //more or less than limit then error thrown-ArrayIndexOutOfBoundsException
+        //System.out.println(marks[10]); // .ArrayIndexOutOfBoundsException
+
+
+
+    }
+}
